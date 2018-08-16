31aeb30ed3a13b8186ae4424a14a6a8a2d409cba
Aaron <aabaker99@gmail.com>
2018-08-16 13:28:35 -0500

    support more than 2 latent-factor-producing inputs

--- script/plot_pr_curve.py
+++ script/plot_pr_curve.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-import argparse
+import argparse, sys
 import os, os.path
 import numpy as np
 import sklearn
@@ -13,15 +13,35 @@ def main():
   parser = argparse.ArgumentParser(description="""
 Evaluate NMF versus Pathway-Regularized Matrix Factorization by plotting PR curves on one figure.
 """)
-  parser.add_argument("--nmf-gene-by-latent", type=str, required=True, help=".csv file")
-  parser.add_argument("--prmf-gene-by-latent", type=str, required=True, help=".csv file")
+  parser.add_argument("--nmf-gene-by-latent", type=str, help=".csv file")
+  parser.add_argument("--prmf-gene-by-latent", type=str, help=".csv file")
+  parser.add_argument("--plier-gene-by-latent", type=str, help=".csv file")
   parser.add_argument("--nodelist", type=argparse.FileType('r'), required=True)
   parser.add_argument("--true-seeds", type=argparse.FileType('r'), required=True)
   parser.add_argument("--outdir", type=str, required=True)
   args = parser.parse_args()
 
-  W_mat_nmf = np.genfromtxt(args.nmf_gene_by_latent, delimiter=",")
-  W_mat_prmf = np.genfromtxt(args.prmf_gene_by_latent, delimiter=",")
+  if args.nmf_gene_by_latent is None and args.prmf_gene_by_latent is None and args.plier_gene_by_latent is None:
+    sys.stderr.write("At least one of --nmf-gene-by-latent, --prmf-gene-by-latent, or --plier-gene-by-latent must be set\n")
+    sys.exit(20)
+
+  # parse inputs - {{
+  W_mats = []
+  label_strs = []
+  colors = []
+  if args.nmf_gene_by_latent is not None:
+    W_mats.append(np.genfromtxt(args.nmf_gene_by_latent, delimiter=","))
+    label_strs.append("NMF; AUC={:0.3f}")
+    colors.append('grey')
+  if args.prmf_gene_by_latent is not None:
+    W_mats.append(np.genfromtxt(args.prmf_gene_by_latent, delimiter=","))
+    label_strs.append("PRMF; AUC={:0.3f}")
+    colors.append('red')
+  if args.plier_gene_by_latent is not None:
+    W_mats.append(np.genfromtxt(args.plier_gene_by_latent, delimiter=","))
+    label_strs.append("PLIER; AUC={:0.3f}")
+    colors.append('blue')
+
   nodelist = fl.parse_nodelist(args.nodelist)
 
   true_seed_fps = []
@@ -36,48 +56,39 @@ Evaluate NMF versus Pathway-Regularized Matrix Factorization by plotting PR curv
       true_seed_lists.append(seed_list)
 
   pathways_mat = fl.nodelists_to_mat(true_seed_lists, nodelist)
+  # }} - parse inputs
   
-  matching_nmf = fl.match(W_mat_nmf, pathways_mat)
-  matching_prmf = fl.match(W_mat_prmf, pathways_mat)
-
-  # transform matching_nmf to a dict keyed by pathway_id for comparison
-  matching_nmf_dict = {}
-  for match in matching_nmf:
-    factor_id, pathway_id, auc = match
-    matching_nmf_dict[pathway_id] = match
+  # reorganize <matching> so we can find each method's latent factor that best matches the ground truth
+  pathway_to_latent_maps = []
+  for i in range(len(W_mats)):
+    matching = fl.match(W_mats[i], pathways_mat)
+    pathway_to_latent_map = {}
+    for match in matching:
+      factor_id_match, pathway_id_match, auc = match
+      factor_id = matching_id_to_ind(factor_id_match)
+      pathway_id = matching_id_to_ind(pathway_id_match)
+      pathway_to_latent_map[pathway_id] = (factor_id, auc)
+    pathway_to_latent_maps.append(pathway_to_latent_map)
 
   # plot Precision-Recall curves
   match_ind = 0
-  for match in matching_prmf:
-    factor_id, pathway_id_match, auc_prmf = match
-    factor_id_prmf = matching_id_to_ind(factor_id)
-    pathway_id = matching_id_to_ind(pathway_id_match)
-
-    factor_id, pathway_id_match2, auc_nmf = matching_nmf_dict[pathway_id_match]
-    factor_id_nmf = matching_id_to_ind(factor_id)
-
-    y_score_prmf = W_mat_prmf[:,factor_id_prmf]
-    y_score_nmf = W_mat_nmf[:,factor_id_nmf]
-    y_true = pathways_mat[:,pathway_id]
-
-    precision_prmf, recall_prmf, thresholds_prmf = sklearn.metrics.precision_recall_curve(y_true, y_score_prmf)
-    precision_nmf, recall_nmf, thresholds_nmf = sklearn.metrics.precision_recall_curve(y_true, y_score_nmf)
-    #auc = sklearn.metrics.average_precision_score(y_true, y_score)
-
+  for pathway_id in range(pathways_mat.shape[1]):
     plt.clf()
-    plt.step(recall_prmf, precision_prmf, color='red', where='post', label="PRMF; AUC={:0.3f}".format(auc_prmf), linewidth=2.0)
-    plt.step(recall_nmf, precision_nmf, color='grey', where='post', label="NMF; AUC={:0.3f}".format(auc_nmf), linewidth=2.0)
-    #plt.fill_between(recall, precision, step='post', alpha=0.2, color='b')
+    y_true = pathways_mat[:,pathway_id]
+    for i in range(len(pathway_to_latent_maps)):
+      pathway_to_latent_map = pathway_to_latent_maps[i]
+      factor_id, auc = pathway_to_latent_map[pathway_id]
+      y_score = W_mats[i][:,factor_id]
+      precision, recall, thresholds = sklearn.metrics.precision_recall_curve(y_true, y_score)
+      plt.step(recall, precision, color=colors[i], where='pre', label=label_strs[i].format(auc), linewidth=2.0)
     plt.xlabel('Recall', fontsize='x-large')
     plt.ylabel('Precision', fontsize='x-large')
     plt.ylim([0.0, 1.05])
     plt.xlim([0.0, 1.0])
-    # TODO print area in legend
-    plt.title('Precision-Recall of PRMF Against NMF'.format(auc), fontsize='xx-large')
+    plt.title('Precision-Recall of PRMF and Friends', fontsize='xx-large')
     plt.legend()
     ofp = os.path.join(args.outdir, "fig{}.png".format(match_ind))
     plt.savefig(ofp, bbox_inches='tight')
-
     match_ind += 1
 
 if __name__ == "__main__":
