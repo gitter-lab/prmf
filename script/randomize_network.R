a1e42bfaf760219b3e6bb0533c63e5988ea28806
Aaron <aabaker99@gmail.com>
2018-10-03 11:40:25 -0500

    update interface to act on directories instead of individual files

--- script/randomize_network.R
+++ script/randomize_network.R
@@ -3,15 +3,45 @@ library(BiRewire)
 library(igraph)
 library(argparse)
 
+# Split file extension off filepath or filename
+# TODO copied from PLIER_wrapper.R
+splitext = function(text) {
+  rv = NA
+  pattern = "([^.]*)([.][^.]*)$"
+  match_data = regmatches(text, regexec(pattern, text))
+  if(length(match_data[[1]]) != 1) {
+    rv = list(match_data[[1]][2], match_data[[1]][3])
+  }
+  rv
+}
+
 main = function() {
   parser = ArgumentParser(description='Randomize edges in a network in a degree-controlled manner')
-  parser$add_argument('--in-network', help='Network to randomize in graphml format', required=T)
-  parser$add_argument('--out-network', help='Randomized network', required=T)
+  parser$add_argument('--indir', help='Network to randomize in graphml format', required=T)
+  parser$add_argument('--outdir', help='Randomized network', required=T)
   args = parser$parse_args()
 
-  G = read_graph(args$in_network, format='graphml')
-  H = birewire.rewire.undirected(G)
-  write_graph(H, args$out_network, format='graphml')
+  ll = list.files(args$indir)
+  for (ifn in ll) {
+    is_graphml = FALSE
+    ifp = file.path(args$indir, ifn)
+    splitext_rv = splitext(basename(ifn))
+    if (!is.na(splitext_rv)) {
+      bn = splitext_rv[1]
+      ext = splitext_rv[2]
+      if (ext == '.graphml') {
+        is_graphml = TRUE
+      }
+    }
+
+    if (is_graphml) {
+      ofp = file.path(args$outdir, ifn)
+
+      G = read_graph(ifp, format='graphml')
+      H = birewire.rewire.undirected(G)
+      write_graph(H, ofp, format='graphml')
+    }
+  }
 }
 
 main()
