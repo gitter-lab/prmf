610bcbad1e3dde275389b34c7cb06e51d0c0d1eb
Aaron <aabaker99@gmail.com>
2018-10-10 15:56:38 -0500

    update old python2 function calls to python3 counterpart, add missing import

--- script/map_ensg_to_ensp.py
+++ script/map_ensg_to_ensp.py
@@ -3,6 +3,7 @@ import os, os.path
 import argparse, sys
 from factorlib import ensembl
 from factorlib.string_db import parse_string_fh
+from factorlib.script_utils import add_file_and_dir_args, check_file_and_dir_args
 
 def main():
   parser = argparse.ArgumentParser()
@@ -16,8 +17,8 @@ def main():
   hgnc_to_ensps_map = ensembl.parse_mapping_tsv(args.mapping_file, key_index=0, value_index=2)
   G = parse_string_fh(args.network_file)
   ensg_to_ensp_map, copy_map = ensembl.filter_one_to_many(hgnc_to_ensps_map, G)
-  for k, v in copy_map.iteritems():
-    sys.stderr.write("[warning] {} ENSPs associated to HGNC:{} found in <network-file>\n".format(v, k))
+  for k, v in copy_map.items():
+    sys.stderr.write("[warning] {} ENSPs associated to {} found in <network-file>\n".format(v, k))
 
   ensembl.apply_mapping(ensg_to_ensp_map, io_pairs)
 
