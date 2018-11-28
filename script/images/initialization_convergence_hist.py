4e46e04e338540c3cd292365769ec17f23f8e140
Aaron <aabaker99@gmail.com>
2018-11-28 11:52:19 -0600

    resolve todo to implement plotting

--- script/images/initialization_convergence_hist.py
+++ script/images/initialization_convergence_hist.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python
 import argparse, sys
-import factorlib as fl
 import re
 import os, os.path
+import numpy as np
+import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib.pyplot as plt
+import factorlib as fl
 
 def main():
   parser = argparse.ArgumentParser()
   parser.add_argument('--indir', help='Directory containing nmf_pathway.py outputs for multiple different runs, each with a different initialization', required=True)
+  parser.add_argument('--outfile', help='image file to create')
   args = parser.parse_args()
 
   # <indir>/run\d+/obj.txt
@@ -26,13 +31,13 @@ def main():
     if os.path.exists(obj_path):
       converge = fl.parse_pathway_obj(obj_path)
     else:
-      sys.stderr.write("[warning] Missing objective output for run with output directory {}\n".format(os.path.basename(obj_path)))
+      sys.stderr.write("[warning] Missing objective output for run with output directory {}\n".format(os.path.basename(run_dir)))
 
     init_path = os.path.join(run_dir, 'init_pathways.txt')
     if os.path.exists(init_path):
       init = fl.parse_init(init_path)
     else:
-      sys.stderr.write("[warning] Missing initialization output for run with output directory {}\n".format(os.path.basename(obj_path)))
+      sys.stderr.write("[warning] Missing initialization output for run with output directory {}\n".format(os.path.basename(run_dir)))
 
     if init is not None and converge is not None:
       # then include results in histogram
@@ -44,17 +49,18 @@ def main():
           count += 1
       counts.append(count)
 
-    if k is None:
-      k = len(init)
+      if k is None:
+        k = len(init)
 
   # make a histogram of counts
   n_bins = k+1
-  n, bins, patches = plt.hist(count, bins=n_bins)
+  n, bins, patches = plt.hist(count, bins=np.arange(n_bins+1)-0.5)
 
   # TODO
-  plt.xlabel()
-  plt.savefig()
-
+  plt.xlabel('Number persisting')
+  plt.ylabel('Frequency')
+  plt.title('Initial pathways persisting in converged set')
+  plt.savefig(args.outfile)
 
 if __name__ == "__main__":
   main()
