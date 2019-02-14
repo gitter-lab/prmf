d1f9b3c504ed82248934a13342fa9f6a212bc5d3
Aaron <aabaker99@gmail.com>
2019-02-14 07:40:55 -0600

    move function to script due to single use, add other measurements

--- script/images/measure_smoothness.py
+++ script/images/measure_smoothness.py
@@ -1,12 +1,101 @@
 #!/usr/bin/env python
 import sys, argparse
 import os, os.path
-import factorlib as fl
+import itertools as it
 import numpy as np
 import networkx as nx
 import matplotlib as mpl
 mpl.use('Agg')
 import matplotlib.pyplot as plt
+import factorlib as fl
+
+def measure_smoothness(V, Gs, nodelist, node_attr='name'):
+  """
+  Parameters
+  ----------
+  V: numpy.array
+    result from nmf_pathway which is a gene x latent factor matrix
+
+  Gs: list of networkx.Graph
+    graphs with a node attribute called <node_attr>, 'name' by default, which is present in <nodelist>
+    list is of length k_latent, matching the size of the array V
+    
+  nodelist: list of str
+    mapping of row index in V to a gene symbol
+
+  Returns
+  -------
+  TODO
+  latent_data : list of list of float
+    The outer list contains data for each latent factor.
+    The inner list contains scores for each edge in the network associated with that latent factor.
+    The edge scores are the absolute differences of node scores for that edge.
+    This is a more detailed expansion of the data which is summarized by the manifold regularization metric.
+  """
+  n_genes, k_latent = V.shape
+
+  node_to_index = {}
+  for i, node in enumerate(nodelist):
+    node_to_index[node] = i
+
+  latent_data = []
+  for k in range(k_latent):
+    G = Gs[k]
+    G = fl.relabel_nodes(G, node_attr)
+
+    edge_diffs = []
+    for u,v in G.edges_iter():
+      u_index = node_to_index[u]
+      v_index = node_to_index[v]
+      edge_diff = abs(V[u_index,k] - V[v_index,k])
+      edge_diffs.append(edge_diff)
+
+    comp_diffs = []
+    comps = sorted(nx.connected_components(G), key=lambda x: len(x), reverse=True)
+    comp = comps[0]
+    for u,v in it.combinations(comp, 2):
+      u_index = node_to_index[u]
+      v_index = node_to_index[v]
+      diff = abs(V[u_index,k] - V[v_index,k])
+      comp_diffs.append(diff)
+
+    all_diffs = []
+    for u,v in it.combinations(G.nodes(), 2):
+      u_index = node_to_index[u]
+      v_index = node_to_index[v]
+      diff = abs(V[u_index,k] - V[v_index,k])
+      all_diffs.append(diff)
+
+    latent_datum = {
+      'edge': edge_diffs,
+      'comp': comp_diffs,
+      'all': all_diffs
+    }
+
+    latent_data.append(latent_datum)
+  return latent_data
+
+def report_smoothness(measures, measure_type):
+  for k in range(len(latent_edge_diffs)):
+    csv_fh = open(os.path.join(args.outdir, 'latent{}_{}.csv'.format(k, measure_type)), 'w')
+    hist_fp = os.path.join(args.outdir, 'latent{}_{}.png'.format(k, measure_type))
+
+    edge_diffs = latent_edge_diffs[k][measure_type]
+    for edge_diff in edge_diffs:
+      csv_fh.write("{}\n".format(edge_diff))
+
+    # log parameter of plt.hist sets the y axis to log scale but not the x axis
+    # change the x axis values to log scale
+    edge_diffs = np.array(edge_diffs)
+    min_str = 'min = {:1.3f}'.format(np.min(edge_diffs)) if edge_diffs.shape[0] > 0 else 'min = NaN'
+    max_str = 'max = {:1.3f}'.format(np.max(edge_diffs)) if edge_diffs.shape[0] > 0 else 'max = NaN'
+
+    plt.clf()
+    n, bins, patches = plt.hist(edge_diffs)
+    plt.xlabel('{} differences'.format(measure_type.capitalize()))
+    plt.ylabel('Frequency')
+    plt.title('Smoothness of pathway edges\n{} ; {}'.format(min_str, max_str))
+    plt.savefig(hist_fp)
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser(description="""
@@ -27,28 +116,12 @@ Plot the results in a histogram.
   Gs = []
   for k in range(k_latent):
     pathway_fp = latent_to_pathway_fp[k]
-    G = nx.read_graphml(pathway_fp)
+    G = nx.read_graphml(pathway_fp).to_undirected()
     Gs.append(G)
 
   nodelist = fl.parse_nodelist(open(args.nodelist))
 
-  latent_edge_diffs = fl.measure_smoothness(V, Gs, nodelist)
-
-  for k in range(len(latent_edge_diffs)):
-    csv_fh = open(os.path.join(args.outdir, 'latent{}.csv'.format(k)), 'w')
-    hist_fp = os.path.join(args.outdir, 'latent{}.png'.format(k))
-
-    edge_diffs = latent_edge_diffs[k]
-    for edge_diff in edge_diffs:
-      csv_fh.write("{}\n".format(edge_diff))
-
-    # log parameter of plt.hist sets the y axis to log scale but not the x axis
-    # change the x axis values to log scale
-    edge_diffs = np.array(edge_diffs)
-
-    plt.clf()
-    n, bins, patches = plt.hist(edge_diffs)
-    plt.xlabel('Edge differences')
-    plt.ylabel('Frequency')
-    plt.title('Smoothness of pathway edges\nmin = {:1.3f} ; max = {:1.3f}'.format(np.min(edge_diffs), np.max(edge_diffs)))
-    plt.savefig(hist_fp)
+  latent_edge_diffs = measure_smoothness(V, Gs, nodelist)
+  report_smoothness(latent_edge_diffs, 'edge')
+  report_smoothness(latent_edge_diffs, 'comp')
+  report_smoothness(latent_edge_diffs, 'all')
