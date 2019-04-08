68e6010ed5ec11066dc461c6dbbea0b11cf41878
Aaron <aabaker99@gmail.com>
2019-04-08 15:56:51 -0500

    change pyNBS wrapper to match corresponding patch to pyNBS to return more data from NBS_single

--- script/pyNBS/pyNBS_wrapper.py
+++ script/pyNBS/pyNBS_wrapper.py
@@ -92,7 +92,6 @@ if __name__ == "__main__":
 
   # graph regularizer
   network = nx.read_graphml(args.network)
-  print(type(network))
   knnGlap = core.network_inf_KNN_glap(network)
 
   # diffusion
@@ -101,25 +100,7 @@ if __name__ == "__main__":
   network_I = pd.DataFrame(np.identity(len(network_nodes)), index=network_nodes, columns=network_nodes)
   kernel = prop.network_propagation(network, network_I, alpha=alpha, symmetric_norm=True)
 
-  # NOTE for benchmarking we are not interested in the consensus clustering result
-  clusters = args.k_latent
-  niter = 1
-  Hlist = []
-  for i in range(niter):
-    netNMF_time = time.time()
-    # Run pyNBS core steps and save resulting H matrix to Hlist
-    H_df = pyNBS_single.NBS_single(arr_df, knnGlap, propNet=network, propNet_kernel=kernel, k=clusters, **save_args)
-    Hlist.append(H_df)
-    ##########################################################################################################
-    # Optional: If the user is saving intermediate outputs (propagation results or H matrices), 
-    # a different 'iteration_label' should be used for each call of pyNBS_single().
-    # Otherwise, the user will overwrite each H matrix at each call of pyNBS_single()
-    # Uncomment and run the two lines below to save intermediate steps instead of the previous line
-    # save_args['iteration_label']=str(i+1)
-    # Hlist.append(pyNBS_single.NBS_single(arr_df, propNet=network, propNet_kernel=kernel, regNet_glap=knnGlap, 
-    #                                      k=clusters, **save_args))
-    ##########################################################################################################
-    # Report run time of each pyNBS iteration
-    t = time.time()-netNMF_time
-    print 'NBS iteration:', i+1, 'complete:', t, 'seconds'
-  #NBS_cc_table, NBS_cc_linkage, NBS_cluster_assign = cc.consensus_hclust_hard(Hlist, k=clusters, **save_args)
+  # Run pyNBS core steps
+  H_df, W, H = pyNBS_single.NBS_single(arr_df, knnGlap, propNet=network, propNet_kernel=kernel, k=args.k_latent, **save_args)
+  np.savetxt(os.path.join(args.outdir, "W.csv"), W, delimiter=",")
+  np.savetxt(os.path.join(args.outdir, "H.csv"), H, delimiter=",")
