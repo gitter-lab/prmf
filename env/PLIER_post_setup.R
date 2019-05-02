62ddff83baf19e81af42931eed5dac7b6269d34b
Aaron Baker <abaker25@wisc.edu>
2019-05-02 14:43:11 -0500

    finalize PLIER environment

--- env/PLIER_post_setup.R
+++ env/PLIER_post_setup.R
@@ -1,3 +1,11 @@
 #!/usr/bin/env Rscript
-library(BiocManager)
-BiocManager::install("wgmao/PLIER")
+library(devtools)
+repos = getOption("repos")
+repos["CRAN"] = "http://cran.us.r-project.org"
+options(repos=repos)
+if (!requireNamespace("BiocManager", quietly = TRUE)) {
+  install.packages("BiocManager")
+}
+BiocManager::install("qvalue")
+# ref the latest commit before PLIERconditional (which breaks install)
+install_github("wgmao/PLIER", ref="afb4ccbf761418535c3e47ec6baeb6bcd8ec716a")
