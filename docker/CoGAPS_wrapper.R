61d1922b02e417668661f3d3a0ea8994561c3864
Aaron <aabaker99@gmail.com>
2019-06-13 15:03:14 -0700

    update to new interface, add required flag for all arguments

--- docker/CoGAPS_wrapper.R
+++ docker/CoGAPS_wrapper.R
@@ -4,22 +4,17 @@ library(CoGAPS)
 
 main = function() {
   parser = ArgumentParser(description="Run CoGAPS")
-  parser$add_argument('--data')
-  parser$add_argument('--k-latent')
-  parser$add_argument('--outdir')
+  parser$add_argument('--data', required=TRUE)
+  parser$add_argument('--k-latent', required=TRUE)
+  parser$add_argument('--outdir', required=TRUE)
   args = parser$parse_args()
 
-  D = as.matrix(read.csv(args$data, header=FALSE))
-  n_obs = dim(D)[1]
-  n_features = dim(D)[2]
+  data = as.matrix(read.csv(args$data, row.names=1))
+  params = new("CogapsParams")
+  params = setParam(params, "nPatterns", args$k_latent)
 
-  # generate stdev measurements by computing the standard deviation for each observation
-  # and using that as the stdev for each feature measurement in that observation
-  row_stdev = apply(D, 1, sd)
-  S = as.matrix(do.call(cbind, replicate(n_features, row_stdev, simplify=FALSE)))
-
-  # D = AP
-  results = CoGAPS(D, S, nFactor = as.numeric(args$k_latent))
+  # data = AP
+  results = CoGAPS(data, params)
   write.csv(results$Amean, file.path(args$outdir, 'A.csv'), row.names=FALSE, col.names=FALSE)
   write.csv(results$Pmean, file.path(args$outdir, 'P.csv'), row.names=FALSE, col.names=FALSE)
 }
