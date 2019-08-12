b0cf0149abb9d70591d0af2857afd5723f7bc9db
Aaron <aabaker99@gmail.com>
2019-08-12 16:04:48 -0700

    add test script and expected output

---
+++ test/script/PRMF/test.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+import networkx as nx
+import subprocess as sp
+import numpy as np
+import pandas as pd
+import os
+from scipy.stats import gamma
+
+np.random.seed(seed=1)
+
+# generate data from gamma distribution
+m_samples = 100
+n_genes = 1000
+k_latent = 6
+
+# X = U * V^T
+# U is m_samples x k_latent
+# V is n_genes x k_latent
+shape_param = 5
+U = gamma.rvs(shape_param, size=m_samples * k_latent).reshape(m_samples, k_latent)
+V = gamma.rvs(shape_param, size=n_genes * k_latent).reshape(n_genes, k_latent)
+X = U.dot(V.transpose())
+nodelist = list(map(lambda x: str(x), range(n_genes)))
+X_df = pd.DataFrame(data=X, index=map(lambda x: str(x), range(m_samples)), columns=nodelist)
+
+# write data
+X_df.to_csv('data.tsv', sep='\t', header=False, index=False)
+
+# write nodelist
+with open('nodelist.txt', 'w') as fh:
+  fh.write('\n'.join(nodelist))
+
+# generate pathways
+manifold_fps = []
+for k in range(k_latent):
+  inds = np.where(V[:,k] > np.percentile(V[:,k], 95))[0]
+  G = nx.generators.path_graph(len(inds))
+
+  # add name attribute to nodes
+  for i in range(len(inds)):
+    G.node[i]['name'] = str(inds[i])
+
+  manifold_fp = 'graph{}.graphml'.format(k)
+  nx.write_graphml(G, manifold_fp)
+  manifold_fps.append(manifold_fp)
+
+sp.check_call(args=['nmf_pathway.py', "--data", "data.tsv", "--manifolds"] + manifold_fps + ["--nodelist", 'nodelist.txt', "--outdir", os.curdir, '--delimiter', '\t', '--seed', "1"])
