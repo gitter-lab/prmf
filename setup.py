8f9ef2d5b4b59144b86cac979b330a5d374aa5d8
Aaron <aabaker99@gmail.com>
2020-03-01 16:59:14 -0600

    rename library

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@
 
 from distutils.core import setup
 
-setup(name="factorlib",
+setup(name="prmf",
   version="0.1",
   description="Library for Pathway-Regularized Matrix Factorization",
   author="Aaron Baker",
   author_email="abaker@cs.wisc.edu",
-  packages=['factorlib']
+  packages=['prmf']
 )
